9cf7656b41a5eb4c8e8072be83ee82341591c85d
Edil Hudaiberdiev <hudaiberdiev.ed@gmail.com>
2024-03-02 13:35:55 +0600

    initial commit for api & frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <>
+      <header>
+          <p>header</p>
+      </header>
+      <main className="mt-5">
+          <p>main</p>
+      </main>
+
+    </>
+  );
+};
+
+export default App;
